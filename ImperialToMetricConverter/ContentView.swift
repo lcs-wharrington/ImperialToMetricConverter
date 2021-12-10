32b48f9c9de66daf08922fd825f8ae9c5c7f6aa5
William Harrington <wharrington@lcs.on.ca>
2021-12-10 08:41:34 -0500

    Initial Commit

---
+++ ImperialToMetricConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImperialToMetricConverter
+//
+//  Created by William Robert Harrington on 2021-12-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImperialToMetricConverter/ImperialToMetricConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImperialToMetricConverterApp.swift
+//  ImperialToMetricConverter
+//
+//  Created by William Robert Harrington on 2021-12-10.
+//
+
+import SwiftUI
+
+@main
+struct ImperialToMetricConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
