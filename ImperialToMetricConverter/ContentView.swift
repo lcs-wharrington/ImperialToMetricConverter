b9fda12913ef7ef3fa6507f3071cc6f0340e5e49
William Harrington <wharrington@lcs.on.ca>
2021-12-10 08:59:58 -0500

    Beginning to build the user's interface. it is displaying a navigation title and all the units the app is converting.

--- ImperialToMetricConverter/ContentView.swift
+++ ImperialToMetricConverter/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored Properties
+    
+    
+    
+    // MARK: Computed Properties
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            
+            HStack{
+                Text("Inches To Centimetres")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+            
+            HStack{
+                Text("Feet To Centimetres")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+            
+            HStack{
+                Text("Yards To Metres")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+            
+            HStack{
+                Text("Miles To Kilometres")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+            
+            
+            Spacer()
+        }
+            .navigationTitle("Imperial To Metric")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }
     }
 }
